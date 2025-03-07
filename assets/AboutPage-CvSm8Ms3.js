fc898ea131b58fb1276fe476a938ae98e9bd2e28
Aleks-corp <Aleks-corp@users.noreply.github.com>
2025-03-07 01:02:48 +0000

    deploy: e3d5a63d8bd0a9b0f2f0866dd2d8a039e5e1404a

---
+++ assets/AboutPage-CvSm8Ms3.js
@@ -0,0 +1,1 @@
+import{j as e}from"./main-CVeF9nba.js";function t(){return e.jsxs("p",{style:{textAlign:"center",marginTop:"30px"},children:[e.jsx("span",{style:{color:"#00e990"},children:"About"})," Content in progress..."]})}t.displayName="AboutPage";export{t as Component};
--- assets/ProjectsPage-euZvFulC.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./main-DQPBzHR7.js";function t(){return e.jsxs("div",{style:{textAlign:"center",marginTop:"30px"},children:[e.jsx("span",{style:{color:"#00e990"},children:"Projects"})," Content in progress..."]})}t.displayName="ProjectsPage";export{t as Component};
