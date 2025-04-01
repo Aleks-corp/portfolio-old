cf91a3f39a4a8a42164ab0db86ba768883a29ce0
Aleks Haran <101347633+Aleks-corp@users.noreply.github.com>
2025-04-02 01:12:01 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { resolve } from "path";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/portfolio",
+  base: "/portfolio-old",
   build: {
     rollupOptions: {
       input: {
